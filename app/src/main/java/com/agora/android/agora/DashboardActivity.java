298f162d67974d3f2502d3ff0573220ca723b478
rohanpb5567 <rohanpb5567@gmail.com>
2018-10-23 16:20:25 -0400

    Add Bottom Navigation View

--- app/src/main/java/com/agora/android/agora/DashboardActivity.java
+++ app/src/main/java/com/agora/android/agora/DashboardActivity.java
@@ -1,8 +1,11 @@
 package com.agora.android.agora;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.Toast;
+import android.support.design.widget.BottomNavigationView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
@@ -14,5 +17,13 @@ public class DashboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
         GoogleSignInAccount gsiAccount = getIntent().getParcelableExtra("Account");
         Toast.makeText(this, gsiAccount.getDisplayName(), Toast.LENGTH_SHORT).show();
-    }
+
+        BottomNavigationView bottom_nav = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottom_nav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return false;
+            }
+        });
+}
 }
