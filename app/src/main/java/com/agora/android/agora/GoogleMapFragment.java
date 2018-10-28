77f066822fe44841d276280719ff0d617594361a
Pranay Methuku <pranaymethuku@gmail.com>
2018-10-27 23:44:44 -0400

    Fix map fragment layout w/ autocomplete

--- app/src/main/java/com/agora/android/agora/GoogleMapFragment.java
+++ app/src/main/java/com/agora/android/agora/GoogleMapFragment.java
@@ -1,18 +1,12 @@
 package com.agora.android.agora;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,13 +15,10 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.Toast;
-
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.AutocompleteFilter;
 import com.google.android.gms.location.places.AutocompletePrediction;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.PlaceBuffer;
@@ -40,8 +31,6 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-import java.util.List;
 
 public class GoogleMapFragment extends Fragment {
 
@@ -139,7 +128,10 @@ public class GoogleMapFragment extends Fragment {
 
         addressText.setOnItemClickListener(autoCompleteListen);
 
-        adapter=new PlaceAutocompleteAdapter(getActivity(),mGoogleApiClient, LAT_LNG_BOUNDS,null);
+        AutocompleteFilter filter =
+                new AutocompleteFilter.Builder().setTypeFilter(AutocompleteFilter.TYPE_FILTER_GEOCODE).build();
+
+        adapter=new PlaceAutocompleteAdapter(getActivity(),mGoogleApiClient, LAT_LNG_BOUNDS,filter);
         addressText.setAdapter(adapter);
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
