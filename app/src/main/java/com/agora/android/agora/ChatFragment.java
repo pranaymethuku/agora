24ebf8608f5260acc470891c791d137fed732531
Pranay Methuku <pranaymethuku@gmail.com>
2018-11-08 22:46:15 -0500

    Clean up Chat fragment a bit

--- app/src/main/java/com/agora/android/agora/ChatFragment.java
+++ app/src/main/java/com/agora/android/agora/ChatFragment.java
@@ -3,8 +3,11 @@ package com.agora.android.agora;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.recyclerview.extensions.ListAdapter;
+import android.support.v7.widget.RecyclerView;
 import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,37 +16,27 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
-//import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-//import com.google.firebase.database.FirebaseDatabase;
+//
+//import com.google.firebase.firestore.CollectionReference;
+//import com.google.firebase.firestore.DocumentReference;
+//import com.google.firebase.firestore.FirebaseFirestore;
+//import com.google.firebase.firestore.Query;
+//import com.google.firebase.firestore.QueryDocumentSnapshot;
+//import com.google.firebase.firestore.QuerySnapshot;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ChatFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ChatFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ChatFragment extends Fragment {
     private static String ARG_GOOGLE_SIGN_IN_ACCOUNT = "com.agora.android.agora.google_sign_in_account";
 
     private GoogleSignInAccount mGoogleSignInAccount;
-    private OnFragmentInteractionListener mListener;
 
-//    private FirebaseListAdapter<ChatMessage> adapter;
+    private String mChannelID = "com.agora.android.agora.channel_id";
+    private String mRoomName = "com.agora.android.agora.observable_room";
+    private EditText mMessageInput;
 
     public ChatFragment() {
         // Required empty public constructor
-    }
-
-    public static ChatFragment newInstance(GoogleSignInAccount googleSignInAccount) {
-        ChatFragment fragment = new ChatFragment();
-        Bundle args = new Bundle();
-        args.putParcelable(ARG_GOOGLE_SIGN_IN_ACCOUNT, googleSignInAccount);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
@@ -53,6 +46,7 @@ public class ChatFragment extends Fragment {
             mGoogleSignInAccount = getArguments().getParcelable(ARG_GOOGLE_SIGN_IN_ACCOUNT);
         }
 
+
     }
 
     @Override
@@ -61,98 +55,8 @@ public class ChatFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_chat, container, false);
 
-        FloatingActionButton fab =
-                (FloatingActionButton) view.findViewById(R.id.fab);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EditText input = (EditText) view.findViewById(R.id.input);
-
-                // Read the input field and push a new instance
-                // of ChatMessage to the Firebase database
-//                FirebaseDatabase.getInstance()
-//                        .getReference()
-//                        .push()
-//                        .setValue(new ChatMessage(input.getText().toString(),
-//                                mGoogleSignInAccount.getId())
-//                        );
-
-                // Clear the input
-//                input.setText("");
-////                input.
-            }
-        });
+        mMessageInput = (EditText) view.findViewById(R.id.message_input);
 
         return view;
-    }
-
-    private void displayChatMessages() {
-        View view = getView();
-        ListView listOfMessages = (ListView) view.findViewById(R.id.list_of_messages);
-//
-//        adapter = new FirebaseListAdapter<ChatMessage>(getActivity(), ChatMessage.class, R.layout.messages, FirebaseDatabase.getInstance().getReference()) {
-//            @Override
-//            protected void populateView(View v, ChatMessage model, int position) {
-//                // Get references to the views of message.xml
-//                TextView messageText = (TextView)v.findViewById(R.id.message_text);
-//                TextView messageUser = (TextView)v.findViewById(R.id.message_user);
-//                TextView messageTime = (TextView)v.findViewById(R.id.message_time);
-//
-//                // Set their text
-//                messageText.setText(model.getMessageText());
-//                messageUser.setText(model.getMessageUser());
-//
-//                // Format the date before showing it
-//                messageTime.setText(DateFormat.format("dd-MM-yyyy (HH:mm:ss)",
-//                        model.getMessageTime()));
-//            }
-//        };
-
-//        listOfMessages.setAdapter(adapter);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-
-    public OnFragmentInteractionListener getListener() {
-        return mListener;
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
