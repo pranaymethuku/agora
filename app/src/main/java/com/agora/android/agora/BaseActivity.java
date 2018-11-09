074950febe7ccad878534d21bc7246c6cd994b9d
Pranay Methuku <pranaymethuku@gmail.com>
2018-11-08 22:45:50 -0500

    Change workflow to launch BaseActivity first

--- app/src/main/java/com/agora/android/agora/BaseActivity.java
+++ app/src/main/java/com/agora/android/agora/BaseActivity.java
@@ -3,55 +3,63 @@ package com.agora.android.agora;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.places.Places;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.List;
 
 public class BaseActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
-    private Button mSignOutButton;
+    private static final String TAG = "BaseActivity";
     private BottomNavigationView mBottomNavigationView;
     private int mCurrentItemID;
+
+    private SharedPreferences mSharedPreferences;
+
     private GoogleApiClient mGoogleApiClient;
 
-
-    private static final String EXTRA_GOOGLE_ACCOUNT = "com.agora.android.agora.google_account";
-    private static final String EXTRA_SIGN_OUT = "com.agora.android.agora.sign_out";
-
-    public static boolean isSignOutRequested(Intent intent) {
-        if (intent == null) {
-            return false;
-        }
-        return intent.getBooleanExtra(EXTRA_SIGN_OUT, false);
-    }
+    // Firebase instance variables
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
-        final GoogleSignInAccount googleSignInAccount = getIntent()
-                .getParcelableExtra(EXTRA_GOOGLE_ACCOUNT);
+        // Initialize Firebase Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        if (mFirebaseUser == null) {
+            // Not signed in, launch the Auth activity
+            startActivity(new Intent(this, AuthActivity.class));
+            finish();
+            return;
+        }
 
         final FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
@@ -63,7 +71,7 @@ public class BaseActivity extends AppCompatActivity implements GoogleApiClient.O
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 if (mCurrentItemID != item.getItemId()) {
-                    Fragment itemFragment = getItemFragment(item, googleSignInAccount);
+                    Fragment itemFragment = getItemFragment(item);
                     if (itemFragment == null) {
                         Toast.makeText(BaseActivity.this, "ERROR: You cannot visit " + item.getTitle(), Toast.LENGTH_SHORT).show();
                         return false;
@@ -88,30 +96,18 @@ public class BaseActivity extends AppCompatActivity implements GoogleApiClient.O
                 .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
                 .addApi(Places.GEO_DATA_API)
                 .addApi(Places.PLACE_DETECTION_API)
+                .addApi(Auth.GOOGLE_SIGN_IN_API)
                 .build();
-//
-//
-//        mSignOutButton = (Button) findViewById(R.id.button_sign_out);
-//        mSignOutButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                setSignOutRequested(true);
-//            }
-//        });
     }
 
-    public GoogleApiClient getGoogleApiClient() {
-        return mGoogleApiClient;
-    }
-
-    private Fragment getItemFragment(MenuItem item, GoogleSignInAccount googleSignInAccount) {
+    private Fragment getItemFragment(MenuItem item) {
         Fragment itemFragment = null;
         switch (item.getItemId()) {
             case R.id.nav_dashboard:
-                itemFragment = DashboardFragment.newInstance(googleSignInAccount);
+                itemFragment = new DashboardFragment();
                 break;
             case R.id.nav_chat:
-                itemFragment = ChatFragment.newInstance(googleSignInAccount);
+                itemFragment = new ChatFragment();
                 break;
             case R.id.nav_map:
                 Location location = getLastLocation();
@@ -123,17 +119,34 @@ public class BaseActivity extends AppCompatActivity implements GoogleApiClient.O
         return itemFragment;
     }
 
-    public static Intent newIntent(Context context, GoogleSignInAccount googleSignInAccount) {
-        Intent intent = new Intent(context, BaseActivity.class);
-        intent.putExtra(EXTRA_GOOGLE_ACCOUNT, googleSignInAccount);
-        return intent;
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if user is signed in.
+        // TODO: Add code to check if user is signed in.
     }
 
-    private void setSignOutRequested(boolean isSignOutRequested) {
-        Intent data = new Intent();
-        data.putExtra(EXTRA_SIGN_OUT, isSignOutRequested);
-        setResult(RESULT_OK, data);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.top_options, menu);
+        return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.options_sign_out:
+                mFirebaseAuth.signOut();
+                Auth.GoogleSignInApi.signOut(mGoogleApiClient);
+                startActivity(new Intent(this, AuthActivity.class));
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 
     private Location getLastLocation() {
         boolean permissionOn=false;
@@ -166,6 +179,9 @@ public class BaseActivity extends AppCompatActivity implements GoogleApiClient.O
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
     }
 }
